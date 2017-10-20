e45aac51b4a90b26ba1d7c3413db560823e27035
Andre Thomas <andre.thomas@modea.com>
2017-10-19 20:46:34 -0400

    change projects from inline-block to float left

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -10,6 +11,9 @@ import { EducationComponent } from './education/education.component';
 import { ContactComponent } from './contact/contact.component';
 import { FooterComponent } from './footer/footer.component';
 import { SkillCircleComponent } from './skill-circle/skill-circle.component';
+import { Project1Component } from './projects/project1/project1.component';
+import { Project2Component } from './projects/project2/project2.component';
+import { Project3Component } from './projects/project3/project3.component';
 
 @NgModule({
   declarations: [
@@ -21,10 +25,27 @@ import { SkillCircleComponent } from './skill-circle/skill-circle.component';
     EducationComponent,
     ContactComponent,
     FooterComponent,
-    SkillCircleComponent
+    SkillCircleComponent,
+    Project1Component,
+    Project2Component,
+    Project3Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: 'project1',
+        component: Project1Component
+      },
+      {
+        path: 'project2',
+        component: Project2Component
+      },
+      {
+        path: 'project3',
+        component: Project3Component
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
