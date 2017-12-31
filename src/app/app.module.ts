f2c2b581655c8b27cfca0838b8e226a2f080176d
Andre <athomas3@vt.edu>
2017-12-31 04:31:58 +0100

    Remove skills section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,32 +5,30 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AboutComponent } from './about/about.component';
-import { SkillsComponent } from './skills/skills.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { EducationComponent } from './education/education.component';
 import { ContactComponent } from './contact/contact.component';
 import { FooterComponent } from './footer/footer.component';
-import { SkillCircleComponent } from './skill-circle/skill-circle.component';
 import { Project1Component } from './projects/project1/project1.component';
 import { Project2Component } from './projects/project2/project2.component';
 import { Project3Component } from './projects/project3/project3.component';
 import {SvgComponent } from './shared/components/svg/svg.component';
+import { SkillComponent } from './about/skill/skill.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     AboutComponent,
-    SkillsComponent,
     PortfolioComponent,
     EducationComponent,
     ContactComponent,
     FooterComponent,
-    SkillCircleComponent,
     Project1Component,
     Project2Component,
     Project3Component,
-    SvgComponent
+    SvgComponent,
+    SkillComponent
   ],
   imports: [
     BrowserModule,
